2c90c66e8bf14b65d2664779775c8444700eee30
Sergio Rojas <srojas@apexsystems.com>
2025-07-25 11:02:26 -0600

    Add Stories

---
+++ cypress/e2e/Mid-Checkpoint/tabsTestingStory.ts
@@ -0,0 +1,6 @@
+// USER STORY 7 (TABS AND OPEN TESTING)
+// Create the test to validate the functionality of the next page https://demoqa.com/selectable Use basic methods to ensure functionality works
+// ACCEPTANCE CRITERIA:
+// 1. Test 2 tabs List and Grid
+// 2. Everytime you select one element validate its selected and other doesn’t
+// 3. Use class properties or colors for validation of actives and inactive
