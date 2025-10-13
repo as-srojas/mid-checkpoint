f5ad5d74f0c0d4b7bb6955aad55dc6aac1865fb4
Sergio Rojas <srojas@apexsystems.com>
2025-10-13 14:20:46 -0600

    Checkbox Story

--- cypress/e2e/Mid-Checkpoint/checkBoxStory.cy.ts
+++ cypress/e2e/Mid-Checkpoint/checkBoxStory.cy.ts
@@ -6,9 +6,39 @@ describe('CheckBox Scenarios', () => {
         cy.visit('https://demoqa.com/checkbox');
     })
   
-    it('First Scenario', () => {
-
+    it('Check And Uncheck All Boxes', () => {
+        checkBoxPage.clickOnExpandAllButton();
+        checkBoxPage.checkAllBoxes();
+        checkBoxPage.isAllBoxesChecked();
+        checkBoxPage.uncheckAllBoxes();
+        checkBoxPage.isAllBoxesUnchecked();
     })
   
+    it('Check Principal Boxes', () => {
+        checkBoxPage.clickOnExpandAllButton();        
+        checkBoxPage.checkAndVerifySpecificBox("Desktop");
+        checkBoxPage.checkAndVerifySpecificBox("Documents");
+        checkBoxPage.checkAndVerifySpecificBox("Downloads");
+        checkBoxPage.isAllBoxesChecked();
+    })
+
+    it('Check Specific Boxes', () => {
+        checkBoxPage.clickOnExpandAllButton();        
+        checkBoxPage.checkAndVerifySpecificBox("Word File.doc");
+        checkBoxPage.checkAndVerifySpecificBox("General");
+        checkBoxPage.checkAndVerifySpecificBox("Angular");
+        checkBoxPage.checkAndVerifySpecificBox("Private");
+        checkBoxPage.checkAndVerifySpecificBox("Commands");
+        checkBoxPage.checkAndVerifySpecificBox("Excel File.doc");
+    })
+
+    it('UnCheck Specific Boxes', () => {
+        checkBoxPage.clickOnExpandAllButton();        
+        checkBoxPage.checkAllBoxes();
+        checkBoxPage.uncheckAndVerifySpecificBox("General");
+        checkBoxPage.uncheckAndVerifySpecificBox("Angular");
+        checkBoxPage.uncheckAndVerifySpecificBox("Private");
+        checkBoxPage.uncheckAndVerifySpecificBox("Commands");
+    })
 })
   
--- cypress/page-objects/MidCheckPointPages/checkBoxPage.ts
+++ cypress/page-objects/MidCheckPointPages/checkBoxPage.ts
@@ -1,10 +1,76 @@
 class CheckBoxPage {
-
-    locator = '//h5[text()="Forms"]';
+    expandAllBtn = '//button[@title="Expand all"]';
+    generalUncheckboxSelector = '(//*[local-name()="svg" and contains(@class, "rct-icon-uncheck")])';
+    generalCheckedboxSelector = '(//*[local-name()="svg" and contains(@class, "rct-icon-check")])';
+    specificCheckboxSelector = '//span[text()="arg"]';
+    checkedItemsList = '//div[@id="result"]';
     
     //Methods
-    writeMethod(name: string) {
-        cy.get(this.locator).type(name);
+    clickOnExpandAllButton() {
+        cy.xpath(this.expandAllBtn).should('be.visible').click();
+    }
+
+    checkAndVerifySpecificBox(checkboxName: string) {
+        let specificCheckboxSelector = `//span[text()="${checkboxName}"]`;
+        cy.xpath(specificCheckboxSelector).click();
+        
+        cy.xpath(this.checkedItemsList)
+            .invoke('text')
+            .then((text) => {
+                if(checkboxName.includes('Word File')) expect(text).to.contain("wordFile"); return; 
+                if(checkboxName.includes('Excel File')) expect(text).to.contain("excelFile"); return; 
+                
+                expect(text).to.contain(checkboxName);
+            });
+    }
+
+    uncheckAndVerifySpecificBox(checkboxName: string) {
+        let specificCheckboxSelector = `//span[text()="${checkboxName}"]`;
+        cy.xpath(specificCheckboxSelector).click();
+        
+        cy.xpath(this.checkedItemsList)
+            .invoke('text')
+            .then((text) => {
+                if(checkboxName.includes('Word File')) expect(text).to.not.contain("wordFile"); return; 
+                if(checkboxName.includes('Excel File')) expect(text).to.not.contain("excelFile"); return; 
+                
+                expect(text).to.not.contain(checkboxName);
+            });
+    }
+
+    checkAllBoxes() {
+        let uncheckboxSelector  = this.generalUncheckboxSelector + `[1]`;
+        cy.xpath(uncheckboxSelector).should('be.visible')
+                                                .click();
+        cy.wait(3000);
+    }
+
+    uncheckAllBoxes() {
+        let checkboxSelector  = this.generalCheckedboxSelector + `[1]`;
+        cy.xpath(checkboxSelector).should('be.visible')
+                                                .click();
+        
+        cy.wait(3000);
+    }
+
+    isAllBoxesChecked() {
+        let count = 0;
+        cy.xpath(this.generalCheckedboxSelector + `[*]`).each(($el) => {
+            expect($el).to.have.class('rct-icon-check');
+            if ($el.hasClass('rct-icon-check')) count++;
+        }).then(() => {
+            expect(count).to.equal(17); 
+        });
+    }
+
+    isAllBoxesUnchecked() {
+        let count = 0;
+        cy.xpath(this.generalUncheckboxSelector + `[*]`).each(($el) => {
+            expect($el).to.have.class('rct-icon-uncheck');
+            if ($el.hasClass('rct-icon-uncheck')) count++;
+        }).then(() => {
+            expect(count).to.equal(17); 
+        });
     }
 
 }
