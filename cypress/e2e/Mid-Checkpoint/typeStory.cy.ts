c0c2cdc638176b1f93fee34ddf6830d6364f3c91
Sergio Rojas <srojas@apexsystems.com>
2025-10-14 19:28:48 -0600

    Text Areas Tests

--- cypress/e2e/Mid-Checkpoint/typeStory.cy.ts
+++ cypress/e2e/Mid-Checkpoint/typeStory.cy.ts
@@ -2,12 +2,39 @@
 import typePage from '../../page-objects/MidCheckPointPages/typePage';
 
 describe('Type Scenarios', () => {
+
     beforeEach(() => {
-        cy.visit('https://demoqa.com/text-box');
+        cy.fixture('testData').as('data')
+        cy.visit('text-box');
     })
   
-    it('First Scenario', () => {
-        
+    it("Verify Placeholders", () => {
+        typePage.verifyPlaceholders();
+    })
+
+    it.only('Verify Invalid Email', () => {
+        const dataUser = {
+            fullname: 'sergio_rojas',
+            email: 'sergioexample.com',
+            currentAddress: 'Av. México 123',
+            permanentAddress: 'Av. La Paz 321'
+        };
+
+        typePage.writeOnUsernameInput(dataUser.fullname);
+        typePage.writeOnUserEmailInput(dataUser.email);
+        typePage.writeOnCurrentAddressInput(dataUser.currentAddress);
+        typePage.writeOnPermanentAddressInput(dataUser.permanentAddress);
+        typePage.clickOnSubmitButtonWithInvalidData(dataUser);
+    })
+
+    it('Verify Texts Inputs', function () {
+        for (var i = 0; i < this.data.textBoxData.length; i++) {
+            typePage.writeOnUsernameInput(this.data.textBoxData[i].fullname);
+            typePage.writeOnUserEmailInput(this.data.textBoxData[i].email);
+            typePage.writeOnCurrentAddressInput(this.data.textBoxData[i].currentAddress);
+            typePage.writeOnPermanentAddressInput(this.data.textBoxData[i].permanentAddress);
+            typePage.clickOnSubmitButton(this.data.textBoxData[i]);
+        }
     })
   
 })
--- cypress/page-objects/MidCheckPointPages/typePage.ts
+++ cypress/page-objects/MidCheckPointPages/typePage.ts
@@ -1,10 +1,60 @@
 class TypePage {
 
-    locator = '//h5[text()="Forms"]';
+    userNameInput = '//input[@id="userName"]';
+    emailInput = '//input[@id="userEmail"]';
+    currentAddressInput ='//textarea[@id="currentAddress"]';
+    permaAddressInput = '//textarea[@id="permanentAddress"]';
+    submitBtn = '//button[@id="submit"]';
+    outputMessage = '//div[@id="output"]//div';
+    outputName = '//p[@id="name"]';
+    outputEmail = '//p[@id="email"]';
+    outputCurrentAddress = '//p[@id="currentAddress"]';
+    outputPermaAddress = '//p[@id="permanentAddress"]';
     
     //Methods
-    writeMethod(name: string) {
-        cy.get(this.locator).type(name);
+    verifyPlaceholders() {
+        cy.xpath(this.userNameInput).invoke('attr', 'placeholder').should('eq', 'Full Name')
+        cy.xpath(this.emailInput).invoke('attr', 'placeholder').should('eq', 'name@example.com')
+        cy.xpath(this.currentAddressInput).invoke('attr', 'placeholder').should('eq', 'Current Address')
+    }
+
+    writeOnUsernameInput(name: string) {
+        cy.xpath(this.userNameInput).should('be.visible').clear().type(name); 
+        cy.xpath(this.userNameInput).invoke('val').should('eq', name);
+    }
+
+    writeOnUserEmailInput(email: string) {
+        cy.xpath(this.emailInput).should('be.visible').clear().type(email);
+        cy.xpath(this.emailInput).invoke('val').should('eq', email);
+    }
+
+    writeOnCurrentAddressInput(currentAddress: string) {
+        cy.xpath(this.currentAddressInput).should('be.visible').clear().type(currentAddress);
+        cy.xpath(this.currentAddressInput).invoke('val').should('eq', currentAddress);
+    }
+
+    writeOnPermanentAddressInput(permaAddress: string) {
+        cy.xpath(this.permaAddressInput).should('be.visible').clear().type(permaAddress);
+        cy.xpath(this.permaAddressInput).invoke('val').should('eq', permaAddress);
+    }
+
+    clickOnSubmitButton(data: string[]) {
+        cy.xpath(this.submitBtn).should('be.visible').click();
+
+        cy.xpath(this.outputName).should('contain', data.fullname);
+        cy.xpath(this.outputEmail).should('contain', data.email);
+        cy.xpath(this.outputCurrentAddress).should('contain', data.currentAddress);
+        cy.xpath(this.outputPermaAddress).should('contain', data.permanentAddress);
+    }
+
+    emailErrorDisplay() {
+        cy.xpath(this.emailInput).should('have.class', 'field-error')
+    }
+
+    clickOnSubmitButtonWithInvalidData(data: any) {
+        cy.xpath(this.submitBtn).should('be.visible').click();
+        this.emailErrorDisplay();
+        cy.xpath(this.outputMessage).should('be.empty');
     }
 
 }
